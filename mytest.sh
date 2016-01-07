758158c3b6600d88716a9978866ca40053592197
Susan Litzinger <susan@psc.edu>
2016-01-07 21:15:48 +0000

    iozone test to run in cron to see how slash2 runs on each client

--- mytest.sh
+++ mytest.sh
@@ -2,4 +2,4 @@
 
 cd /tarc/usr
 mkdir `ifconfig eth0 | grep "inet " | awk '{print $2}'`
-iozone -a >& /tarc/usr/`ifconfig eth0 | grep "inet " | awk '{print $2}'`/test.`ifconfig eth0 | grep "inet " | awk '{print $2}'`.`date +%Y%m%d%H%M%S` 
+iozone -az -i 0 >& /tarc/usr/`ifconfig eth0 | grep "inet " | awk '{print $2}'`/test.`ifconfig eth0 | grep "inet " | awk '{print $2}'`.`date +%Y%m%d%H%M%S` 
